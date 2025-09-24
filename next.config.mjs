87c962108f5a9d1c3bad038ac47177ec9a564068
ARSukhadiya <abhisheksukhadiya3899@gmail.com>
2025-09-24 15:11:44 -0700

    fixed error

--- next.config.mjs
+++ next.config.mjs
@@ -5,16 +5,19 @@ const withMDX = mdx({
   options: {},
 });
 
+const isProd = process.env.NODE_ENV === 'production';
+// const repoName = 'abhishek-sukhadiya-2'; 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
   // Export the project as static HTML
   output: 'export',
 
-  // // Set the base path for assets like CSS and images
-  // // This is crucial for GitHub Pages deployments
-  // basePath: isProd ? `/${repoName}` : '',
-  // assetPrefix: isProd ? `/${repoName}/` : '',
+  // Set the base path for assets like CSS and images
+  // This is crucial for GitHub Pages deployments
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
@@ -31,6 +34,12 @@ const nextConfig = {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
   },
+
+  // Disable Image Optimization API for static exports
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default withMDX(nextConfig);
